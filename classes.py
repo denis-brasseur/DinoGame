cc027e13397dfb22b5f264cbd38e36c2363b1d54
Denis <denis.brasseur@yahoo.fr>
2019-11-05 17:09:04 +0100

    test d'objets bougeant à différentes vitesses

--- classes.py
+++ classes.py
@@ -42,22 +42,24 @@ class Obstacle (pygame.sprite.Sprite) :
 		self.rect.move_ip(x,0)
 		
 	def is_out(self,limites):
-		x,y = self.rect.center
-		if x<0 or x>limites.get_width():
+		if self.rect.right<0 or self.rect.left>limites.get_width():
 			return True
-		if y<0 or y>limites.get_height():
+		if self.rect.bottom<0 or self.rect.top>limites.get_height():
 			return True
 		return False
 
 
 if __name__ == "__main__" :
 	pygame.display.init()
+	pygame.font.init()
 	
 	fenetre = pygame.display.set_mode((0,0))
 	
 	img_fond = pygame.image.load(chemin+"/forest/forest_constructor_kit_READY/10_backgrounds/10_backgrounds-01.jpg").convert()
 	img_fond = pygame.transform.smoothscale(img_fond,(img_fond.get_width()//4,img_fond.get_height()//4))
 	fenetre = pygame.display.set_mode(img_fond.get_size(),RESIZABLE)
+	fond = pygame.Surface(img_fond.get_size())
+	fond.fill((255,255,255))
 	
 	img_obstacle = pygame.image.load(chemin+"/obstacle_1.png").convert_alpha()
 	img_obstacle = pygame.transform.smoothscale(img_obstacle,(img_obstacle.get_width()//10,img_obstacle.get_height()//10))
@@ -66,13 +68,30 @@ if __name__ == "__main__" :
 	
 	img_dino = pygame.image.load(chemin+"/dinosaure_1.png").convert_alpha()
 	img_dino = pygame.transform.smoothscale(img_dino,(img_dino.get_width()//4,img_dino.get_height()//4))
-	perso = Dino(img_dino,(img_dino.get_width(),img_fond.get_height()-img_dino.get_height()))
+	perso = Dino(img_dino,(img_fond.get_width()//3,img_fond.get_height()-img_dino.get_height()))
 	
-	fenetre.blit(img_fond,(0,0))
+	img_rocher = pygame.image.load(chemin+"/rocher_1.png").convert_alpha()
+	img_rocher = pygame.transform.smoothscale(img_rocher,(img_rocher.get_width()//10,img_rocher.get_height()//10))
+	rocher = Obstacle(img_rocher,(fenetre.get_width()-img_rocher.get_width(),fenetre.get_height()-img_rocher.get_height()))
+	
+	img_arbre = pygame.image.load(chemin+"/arbre_1.png").convert_alpha()
+	img_arbre = pygame.transform.smoothscale(img_arbre,(int(img_arbre.get_width()/1.5),int(img_arbre.get_height()/1.5)))
+	arbre = Obstacle(img_arbre,(fenetre.get_width(),fenetre.get_height()-img_arbre.get_height()-10))
+	
+	#fenetre.blit(img_fond,(0,0))
+	fenetre.blit(fond,(0,0))
+	fenetre.blit(arbre.image,(arbre.rect.x,arbre.rect.y))
 	lObstacle.draw(fenetre)
 	perso.draw(fenetre)
-	
+	score = 0
+	namefont = pygame.font.match_font('ComicSansMS')
+	font = pygame.font.Font(namefont,50)
+	text_score = font.render(str(score),True,(0,0,0))
+	fenetre.blit(text_score,(5,fenetre.get_width()-text_score.get_width()-5))
+	fenetre.blit(rocher.image,(rocher.rect.x,rocher.rect.y))
 	pygame.display.flip()
+	
+	
 	
 	timer = pygame.time.Clock()
 	sortir = False
@@ -80,8 +99,6 @@ if __name__ == "__main__" :
 	while not sortir :
 		collision = False
 		timer.tick(30)
-		## créer, ou pas, un nouvel obstacle
-		
 		## attendre un evenement
 		for event in pygame.event.get() :
 			if event.type ==QUIT:
@@ -91,7 +108,7 @@ if __name__ == "__main__" :
 				if event.key == K_UP:
 					perso.impulsion(fenetre)
 			if event.type == USEREVENT:
-				if randrange(3)==0 and len(lObstacle.sprites())<2:
+				if randrange(2)==0 and len(lObstacle.sprites())<2:
 					lObstacle.add(Obstacle(img_obstacle,(fenetre.get_width()-img_obstacle.get_width(),fenetre.get_height()-img_obstacle.get_height())))
 		## bouger les sprites
 		perso.move(fenetre)
@@ -99,13 +116,28 @@ if __name__ == "__main__" :
 			o.move(dx)
 			if o.is_out(fenetre):
 				lObstacle.remove(o)
+				score +=1
+				if float(score//10) == score/10 : 
+					dx -= 10
 		## tester l'existence d'une collision
 		if len(pygame.sprite.spritecollide(perso,lObstacle,False))>0:
 			collision = True
 		## dessiner les objets
-		fenetre.blit(img_fond,(0,0))
+		rocher.move(1.1*dx)
+		arbre.move(0.9*dx)
+		#fenetre.blit(img_fond,(0,0))
+		fenetre.blit(fond,(0,0))
+		fenetre.blit(arbre.image,(arbre.rect.x,arbre.rect.y))
 		lObstacle.draw(fenetre)
 		perso.draw(fenetre)
+		text_score = font.render(str(score),False,(0,0,0))
+		fenetre.blit(text_score,(5,5))
+		fenetre.blit(rocher.image,(rocher.rect.x,rocher.rect.y))
+		if rocher.is_out(fenetre):
+			rocher.rect.x = fenetre.get_width()-rocher.image.get_width()
+		if arbre.is_out(fenetre):
+			arbre.rect.x = fenetre.get_width()
+			arbre.rect.x = fenetre.get_width()
 		pygame.display.flip()
 		## si collision, freeze et quitte
 		if collision :
