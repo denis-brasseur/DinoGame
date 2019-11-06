b12520ff9df35af00979c31fb07486306cf63eab
Denis <denis.brasseur@yahoo.fr>
2019-11-06 12:28:39 +0100

    implémentation d'un score en fonction de la distance, et d'une accélération en fonction du score

--- constantes.py
+++ constantes.py
@@ -7,13 +7,14 @@ fond = "fond.jpg"
 dinosaure = "dinosaure_1.png"
 obstacle = "obstacle_1.png"
 
-largeur_fenetre = 1080
-hauteur_fenetre = 720
+largeur_fenetre = 1600
+hauteur_fenetre = 900
 
-cst_saut = 100
-cst_gravite = 10
-dx = -10
-cst_tolerance = 35
+cst_saut = hauteur_fenetre//10
+cst_gravite = hauteur_fenetre//100
+dx = -(largeur_fenetre//100)
+cst_tolerance = (largeur_fenetre//10)//3
 max_obstacles = 3
-
-
+taille_lettres = 50
+namefont = 'Arial'
+acceleration = 1
